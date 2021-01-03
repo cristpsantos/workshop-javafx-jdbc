6cfe3a9fb8c77ca765e1796dc8dfe52726caecbf
Cristiano Pontes <pontescr@gmail.com>
2021-01-03 00:58:36 -0300

    TextField & DatePicker

--- src/gui/SellerFormController.java
+++ src/gui/SellerFormController.java
@@ -1,8 +1,11 @@
 package gui;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
@@ -17,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Seller;
@@ -36,6 +40,15 @@ public class SellerFormController implements Initializable {
 
 	@FXML
 	private TextField txName;
+	
+	@FXML
+	private TextField txEmail;
+	
+	@FXML
+	private DatePicker dpBirthDate;
+	
+	@FXML
+	private TextField txBaseSalary;
 
 	@FXML
 	private Button btSave;
@@ -46,6 +59,15 @@ public class SellerFormController implements Initializable {
 	@FXML
 	private Label lbError;
 
+	@FXML
+	private Label lbErrorEmail;
+	
+	@FXML
+	private Label lbErrorBirthDate;
+	
+	@FXML
+	private Label lbErrorBaseSalary;
+	
 	public void setSeller(Seller entity) {
 		this.entity = entity;
 	}
@@ -126,7 +148,11 @@ public class SellerFormController implements Initializable {
 
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txId);
-		Constraints.setTextFieldMaxLength(txName, 30);
+		Constraints.setTextFieldMaxLength(txName, 70);
+		Constraints.setTextFieldDouble(txBaseSalary);
+		Constraints.setTextFieldMaxLength(txEmail, 60);
+		Utils.formatDatePicker(dpBirthDate, "dd/MM/yyyy");
+		
 	}
 
 	public void updateFormData() {
@@ -135,5 +161,12 @@ public class SellerFormController implements Initializable {
 		}
 		txId.setText(String.valueOf(entity.getId()));
 		txName.setText(entity.getName());
+		txEmail.setText(entity.getEmail());
+		Locale.setDefault(Locale.US);
+		txBaseSalary.setText(String.format("%.2f", entity.getBaseSalary()));
+		
+		if(entity.getBirthDate() != null ) {
+			dpBirthDate.setValue(LocalDate.ofInstant(entity.getBirthDate().toInstant(), ZoneId.systemDefault()));
+		}
 	}
 }
